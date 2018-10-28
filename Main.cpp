f3fd1bbc9ba4a20aa9bf89b689effe2a197faeaf
Aakash Gupta <aakashgupta@Aakashs-MacBook-Pro.local>
2018-10-27 21:40:09 -0400

    New Main for testing

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,14 @@
 #include "Room.h"
+#include <stdlib.h>
+#include "Parse.h"
 
-int main(char* argv[], int argc) {
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	Parse* parse = new Parse(argv[1]);
+
+for (const auto &p : parse->items) {
+    std::cout << "m[" << p.first << "] = " << p.second << '\n';
+}
 	return EXIT_SUCCESS;
 }
